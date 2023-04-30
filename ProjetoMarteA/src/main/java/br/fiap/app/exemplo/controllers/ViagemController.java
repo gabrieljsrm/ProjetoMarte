93ad015c0b62da569c364c20172506e34b83c971
gabrieljsrm <95320787+gabrieljsrm@users.noreply.github.com>
2023-04-29 22:03:15 -0300

    Initial commit

---
+++ ProjetoMarteA/src/main/java/br/fiap/app/exemplo/controllers/ViagemController.java
@@ -0,0 +1,59 @@
+package br.fiap.app.exemplo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import br.fiap.app.exemplo.repositories.ViagemRepository;
+import br.fiap.app.exemplo.models.Viagem;
+import org.springframework.ui.Model;
+
+
+
+@Controller
+@RequestMapping("/infViagem")
+public class ViagemController {
+
+	@Autowired
+	private ViagemRepository viagemRepository; 
+	
+	@GetMapping("")
+	public ModelAndView Get() {
+		ModelAndView model = new ModelAndView("infViagem/index");
+		List<Viagem> listViagem = viagemRepository.findAll();
+		model.addObject("viagens", listViagem);
+		return model; 
+	}
+	
+	@GetMapping("/create")
+	public ModelAndView Create() {
+		ModelAndView model = new ModelAndView("infViagem/create");
+		return model; 
+	}
+	
+	@PostMapping("/create")
+	public String Create(@ModelAttribute("infViagem")Viagem objViagem) {
+		viagemRepository.save(objViagem);
+		return "redirect:/infViagem";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id")Long id) {
+		viagemRepository.deleteById(id);
+		return "redirect:/infViagem";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String getById(Model model, @PathVariable("id")Integer idViagem) {
+		model.addAttribute("idViagem", idViagem);
+		return "infViagem/edit";
+	}
+	
+	
+}
---
+++ ProjetoMarteA/src/main/java/br/fiap/app/exemplo/models/Viagem.java
@@ -0,0 +1,121 @@
+package br.fiap.app.exemplo.models;
+
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
+
+
+@Entity
+public class Viagem {
+
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.AUTO) // auto increment
+	private Long id;
+
+	
+	private LocalDate dataDecolagem;
+
+	@PositiveOrZero
+	private int duracaoEstadia;
+
+	@PositiveOrZero
+	//@Pattern(regexp = "^([3-9]|[1-9]\\d*[3-9])$")
+	private int assentosDisponiveis;
+
+	@Pattern(regexp = "^[a-z]{4}-\\d{4}$")
+	private String nomeModeloNave;
+
+	@NotNull(message = "O nome do primeiro comandate é obrigatório!")
+	private String nomePrimeiroComandante;
+
+	@Pattern(regexp = "^[a-zA-Z]{3}-\\d{4}-[a-zA-Z]\\d[a-zA-Z]$")
+	private String matriculaPrimeiroComandante;
+
+	@NotNull(message = "O nome do segundo comandate é obrigatório!")
+	private String nomeSegundoComandante;
+
+	@Pattern(regexp = "^[a-zA-Z]{3}-\\d{4}-[a-zA-Z]\\d[a-zA-Z]$")
+	private String matriculaSegundoComandante;
+
+	
+	
+	
+	// getters and setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getDataDecolagem() {
+		return dataDecolagem;
+	}
+
+	public void setDataDecolagem(LocalDate dataDecolagem) {
+		this.dataDecolagem = dataDecolagem;
+	}
+
+	public int getDuracaoEstadia() {
+		return duracaoEstadia;
+	}
+
+	public void setDuracaoEstadia(int duracaoEstadia) {
+		this.duracaoEstadia = duracaoEstadia;
+	}
+
+	public int getAssentosDisponiveis() {
+		return assentosDisponiveis;
+	}
+
+	public void setAssentosDisponiveis(int assentosDisponiveis) {
+		this.assentosDisponiveis = assentosDisponiveis;
+	}
+
+	public String getNomeModeloNave() {
+		return nomeModeloNave;
+	}
+
+	public void setNomeModeloNave(String nomeModeloNave) {
+		this.nomeModeloNave = nomeModeloNave;
+	}
+
+	public String getNomePrimeiroComandante() {
+		return nomePrimeiroComandante;
+	}
+
+	public void setNomePrimeiroComandante(String nomePrimeiroComandante) {
+		this.nomePrimeiroComandante = nomePrimeiroComandante;
+	}
+
+	public String getMatriculaPrimeiroComandante() {
+		return matriculaPrimeiroComandante;
+	}
+
+	public void setMatriculaPrimeiroComandante(String matriculaPrimeiroComandante) {
+		this.matriculaPrimeiroComandante = matriculaPrimeiroComandante;
+	}
+
+	public String getNomeSegundoComandante() {
+		return nomeSegundoComandante;
+	}
+
+	public void setNomeSegundoComandante(String nomeSegundoComandante) {
+		this.nomeSegundoComandante = nomeSegundoComandante;
+	}
+
+	public String getMatriculaSegundoComandante() {
+		return matriculaSegundoComandante;
+	}
+
+	public void setMatriculaSegundoComandante(String matriculaSegundoComandante) {
+		this.matriculaSegundoComandante = matriculaSegundoComandante;
+	}
+
+}
